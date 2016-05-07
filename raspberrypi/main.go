53060b9edbc06b1f458ce4f056f5cb0739ba06ee
Rollie Ma <rollie.ma@gmail.com>
2016-05-07 22:55:38 +0000

    Golang deps and small tweaks to rpi code

--- raspberrypi/main.go
+++ raspberrypi/main.go
@@ -9,31 +9,38 @@ import (
 )
 
 func main() {
-	c := &serial.Config{Name: "/dev/tty.usbmodemFA131", Baud: 9600}
+	talkToArduino()
+}
+
+func talkToArduino() {
+	// c := &serial.Config{Name: "/dev/tty.usbmodemFA131", Baud: 9600}
+	c := &serial.Config{Name: "/dev/ttyACM0", Baud: 115200}
 	s, err := serial.OpenPort(c)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	reader := bufio.NewReader(s)
-	for {
-		_, err = s.Write([]byte("test"))
-		if err != nil {
-			log.Fatal(err)
+	go func() {
+		reader := bufio.NewReader(s)
+		for {
+			reply, _, err := reader.ReadLine()
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println(string(reply))
+			time.Sleep(5 * time.Millisecond)
 		}
-		time.Sleep(time.Second)
+	}()
 
-		// _, err = s.Read(buf)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-		// log.Println(string(buf))
+	_, err = s.Write([]byte("f\n"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		// reply, err := reader.ReadBytes('\x0a')
-		reply, _, err := reader.ReadLine()
-		if err != nil {
-			panic(err)
-		}
-		log.Println(string(reply))
+	time.Sleep(5 * time.Second)
+
+	_, err = s.Write([]byte("s\n"))
+	if err != nil {
+		log.Fatal(err)
 	}
 }
